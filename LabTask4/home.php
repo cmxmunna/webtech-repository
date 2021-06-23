bfbeab98c7ba99c8bc81e072ae27da831b5571c3
cmxmunna <18-36025-1@student.aiub.edu>
2021-06-24 03:48:30 +0600

    home page modified

--- LabTask4/home.php
+++ LabTask4/home.php
@@ -4,36 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PUBLIC HOME</title>
+    <title>IFE Home</title>
     <style>
-        .error {color: #FF0000;}
-        .font {font-family: 'Courier New', Courier, monospace; color: white;}
-        body {background-color: #202020;}
+        body {margin: 0; background-color: #4a4a4a; font-family: 'Courier New', Courier, monospace; color: white;}
         table {border-collapse: collapse; width: 100%;}
-        th {background-color: #101020; color: white; }
-        .tp{padding: 20px;}
-        .link {margin-top: 20px; align: right;}
-        .footer {text-align: center;  padding: 10px;}
+        .error {color: #FF0000;}
+        .footer {position: fixed; left: 0; bottom: 0; width: 100%; background-color: #333; color: white; text-align: center;}
+        .header {overflow: hidden; background-color: #333;}
+        .header a {float: left; display: block; color: cyan; text-align: center; padding: 14px 16px; margin-top: 4px; text-decoration: none;}
+        .header a.logo img {width: auto; height: auto; margin-top: -20px; margin-left: -20px; margin-bottom: -20px; margin-right: -20px;}
+        .header-right {float: right;}
+        .header a:hover {background-color: #4a4a4a; 
+    box-shadow: 10px 10px 20px #000000; text-shadow: 2px 2px 4px #000000; color: red;}
     </style>
 </head>
-<body class="font">
-    <table border="1">
-        <tr>
-            <td height="100px" class="tp" colspan="2">
-                <div align="left"><img src="logo.gif" alt=""></div>
-                <div class="link"><a href="#">Home</a> | <a href="#">Login</a> | <a href="#">Registration</a></div>
-            </td>
-        </tr>
+<body>
+    <div class="header">
+        <a href="home.php" class="logo"><img src="logo.gif" alt="IFE"></a>
+        <div class="header-right">
+            <a href="home.php">Home</a>
+            <a href="#">Login</a>
+            <a href="#">Registration</a>
+        </div>
+    </div>
+    <table border="2">
         <tr>
             <td height="400px">
-                <p> &nbsp;&nbsp;&nbsp;&nbsp;Welcome to IFE.</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <div class="footer"><p>Copyright © 2021 || All Rights Reserved.</p></div>
+                <p> &nbsp;&nbsp;&nbsp;&nbsp; Welcome to IFE.</p>
             </td>
         </tr>
     </table>
+    <div class="footer"><p>Copyright © 2021 || All Rights Reserved.</p></div>
 </body>
 </html>
