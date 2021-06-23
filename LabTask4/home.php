008b45df62b4e986ce4b271dc2e99e672e369aae
cmxmunna <18-36025-1@student.aiub.edu>
2021-06-23 07:17:55 +0600

    Task 4 landing page added

---
+++ LabTask4/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUBLIC HOME</title>
+    <style>
+        .error {color: #FF0000;}
+        .font {font-family: 'Courier New', Courier, monospace; color: white;}
+        body {background-color: #202020;}
+        table {border-collapse: collapse; width: 100%;}
+        th {background-color: #101020; color: white; }
+        .tp{padding: 20px;}
+        .link {margin-top: 20px; align: right;}
+        .footer {text-align: center;  padding: 10px;}
+    </style>
+</head>
+<body class="font">
+    <table border="1">
+        <tr>
+            <td height="100px" class="tp" colspan="2">
+                <div align="left"><img src="logo.gif" alt=""></div>
+                <div class="link"><a href="#">Home</a> | <a href="#">Login</a> | <a href="#">Registration</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td height="400px">
+                <p> &nbsp;&nbsp;&nbsp;&nbsp;Welcome to IFE.</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="footer"><p>Copyright © 2021 || All Rights Reserved.</p></div>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
