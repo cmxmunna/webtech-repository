92aa959adaf0b4d9bb0b14f202235b488a350962
cmxmunna <18-36025-1@student.aiub.edu>
2021-06-26 06:00:23 +0600

    Profile Image Bug Fixed
    Profile Image Bug Fixed, Destination path (folder) added, Login validation updated

--- LabTask3/login.php
+++ LabTask3/login.php
@@ -1,19 +1,36 @@
 <?php
     $userErr = $passErr ="";
     $user = $pass ="";
-    $message = "";  
+    $message = '';  
 
-    if(isset($_POST["submit"]))  
+    if($_SERVER["REQUEST_METHOD"] == "POST") 
     {  
         if(empty($_POST["user"]))  
         {  
             $userErr = "User Name Required!";  
         }
+        else if(strlen($_POST["user"]) <2)
+        {
+            $userErr = "User Name must contain at least two (2) characters";
+        }
+        else
+        {
+            $user = $_POST['user'];
+            if(!preg_match("/^[a-zA-Z0-9_ .-]*$/",$user))
+            {
+                $userErr = "User Name can contain alpha numeric characters, period, dash or underscore only!";
+            }
+
+        }
         
         if(empty($_POST["pass"]))  
         {  
             $passErr = "Password Required!";  
-        }       
+        }  
+        else if(strlen($_POST['pass']) <8)
+        {
+            $passErr = "Password must not be less than eight (8) characters";
+        }  
         else  
         {  
             $message = "<strong>Congratulations! Login Success</strong>";
--- LabTask3/profile_picture.php
+++ LabTask3/profile_picture.php
@@ -1,49 +1,55 @@
 <?php
+$uploadErr="";
+$uploadSuccess="";
 $message="";
-if (isset($_POST["upload"])) 
-{
+$success="";
+$successImg="";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{   
+    $target_file="files/".$_FILES["fileupload"]["name"];
+    $file_extension = pathinfo($_FILES["fileupload"]["name"], PATHINFO_EXTENSION);
     $allowed_image_extension = array(
         "png",
         "jpg",
-        "jpeg"
+        "jpeg",
+        "PNG",
+        "JPG",
+        "JPEG"
     );
-    $file_extension = pathinfo($_FILES["file-input"]["name"], PATHINFO_EXTENSION);
-    if (! in_array($file_extension, $allowed_image_extension)) 
+    if (!in_array($file_extension, $allowed_image_extension)) 
     {
-        $response = array(
-            "type" => "error",
-            "message" => "Upload valid images. Only PNG,JPG and JPEG are allowed."
-        );
-        echo $result;
-    }    
-    else if (($_FILES["file-input"]["size"] > 4000000)) 
-    {
-        $response = array(
-            "type" => "error",
-            "message" => "Image size exceeds 4MB"
-        );
+        $uploadErr = "Upload valid images. Only PNG,JPG and JPEG are allowed.";
     } 
-    else 
+    else if(($_FILES["fileupload"]["size"] > 4000000)) 
     {
-        $messge = "Image uploaded successfully.";
+        $uploadErr= "Image size exceeds 4MB";
+    } 
+     
+    else if(move_uploaded_file($_FILES["fileupload"]["tmp_name"],$target_file))
+    {
+       $uploadSuccess= "you have uploaded ".$_FILES["fileupload"]["name"];
+       $successImg= "<img src=" .$target_file.">";
+       $message = "Image uploaded successfully.";
     }
 }
 ?>
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+    <title>Profile Image</title>
     <style>
         .error {color: #FF0000;}
+        .center {margin-left: auto; margin-right: auto; margin-top: 20px;}
         .font {font-family: 'Courier New', Courier, monospace; color: white;}
         body {background-color: #202020;}
     </style>
 </head>
 <body class="font">
-        <form method="post" action="">
+        <form method="post" action="" enctype="multipart/form-data">
             <fieldset>
                 <legend><strong>PROFILE PICTURE</strong></legend>
                 <table>
@@ -51,23 +57,50 @@ if (isset($_POST["upload"]))
                         <td colspan="2"><img src="https://masged.net/wp-content/themes/cera-child/assets/images/avatars/user-avatar.png" alt="sampleImage" height="180px"></td>
                     </tr>
                     <tr>
-                        <td><input type="file" name="file-input" value="Choose File" style="font-size: 15px;"></td>
+                        <td><input type="file" name="fileupload" value="No file chosen"  style="font-size: 15px;"></td>
+                    </tr>
+                    <tr colspan="2">
+                        <td><span class="error"><?php echo $uploadErr; ?></span></td>
                     </tr>
                     <tr>
                         <td colspan="2">
-                        <?php  
+                            <?php  
                                 if(isset($message))  
                                 {
-                                    echo "<br>" .$message;
+                                    echo $message;
 
-                                } 
+                                }
                             ?>
                         </td>
                     </tr>
                 </table>
                 <br>
-                <input type="submit" name="upload" value="Submit" style="font-size: 15px;">
+                <input type="submit" value="Submit"  style="font-size: 15px;">
             </fieldset>
+            <table class="center">
+                <tr>
+                    <td>
+                        <?php  
+                            if(isset($uploadSuccess))  
+                            {
+                                echo $uploadSuccess;
+
+                            }
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <?php
+                            if(isset($successImg))  
+                            {
+                                echo $successImg;
+
+                            }
+                        ?>
+                    </td>
+                </tr>
+            </table>
         </form>      
     </body>
 </html>
